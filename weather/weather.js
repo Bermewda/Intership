eb1e293da41e676ccf9d2e471b99b2bebcaee388
bermuda-z <bermuda.mew@gmail.com>
2019-01-31 09:50:00 +0700

    add xmltojson

---
+++ weather/weather.js
@@ -0,0 +1,14 @@
+var convert = require('xml-js');
+var args = process.argv.slice(2);
+var xml = require('fs').readFileSync(args[0], 'utf8');
+var result = convert.xml2json(xml, {compact: true, spaces: 4,ignoreDeclaration:true});
+console.log(result)
+
+var fs = require('fs');
+fs.writeFile("weather.json", result, (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    };
+    console.log("File has been created");
+});
